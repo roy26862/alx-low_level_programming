abc155d71f4d477dbf960e4c626020dfea3b5b8e
roy26862 <yusuf26862@gmail.com>
2023-02-19 09:19:51 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char my[] = "\"Programming is like building a multilingual puzzle";
+	puts(my);
+	return (0);
+}
