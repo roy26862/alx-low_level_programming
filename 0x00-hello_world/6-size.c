1c3f22adc0f9b41978de8633b57556d74a8fadf6
roy26862 <yusuf26862@gmail.com>
2023-02-19 09:57:59 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	float f;
+	long int ii;
+	long long ll;
+
+	printf("The size of an int is: %lu\n", (unsigned long)sizeof(i));
+	printf("The size of an char is: %lu\n", (unsigned long)sizeof(c));
+	printf("The size of an float is: %lu\n", (unsigned long)sizeof(f));
+	printf("The size of an long int is: %lu\n", (unsigned long)sizeof(ii));
+	printf("The size of an long long is: %lu\n", (unsigned long)sizeof(ll));
+	
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -10,3 +10,6 @@
 
 5-printf.c - a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
 
+8-size.c - a C program that prints the size of various types on the computer it is compiled and run on.
+
+
